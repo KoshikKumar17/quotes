daf973a2a5f51e385968a82d8596539a2760adb8
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-03-03 13:44:28 +0530

    Update script.js

--- tags/script.js
+++ tags/script.js
@@ -1,36 +1,13 @@
 function getRandomQuote() {
   const category = document.getElementById('category-select').value;
   
-  // Show the loader
-  document.getElementById('loader').style.display = 'block';
-  
   fetch(`https://api.quotable.io/random?tags=${category}`)
     .then(response => response.json())
     .then(data => {
-      // Hide the loader
-      document.getElementById('loader').style.display = 'none';
-      
-      // Clear existing quote
-      document.getElementById('quote-text').innerHTML = '';
-      
-      // Animate new quote
-      new Typed('#quote-text', {
-        strings: [data.content],
-        typeSpeed: 500,
-        showCursor: false,
-      });
-
+      document.getElementById('quote-text').textContent = data.content;
       document.getElementById('quote-author').textContent = '- ' + data.author;
-      
-      // Show the quote
-      document.getElementById('quote-text').classList.remove('hidden');
-      document.getElementById('quote-author').classList.remove('hidden');
     })
-    .catch(error => {
-      console.error('Error fetching quote:', error);
-      // Hide the loader if there's an error
-      document.getElementById('loader').style.display = 'none';
-    });
+    .catch(error => console.error('Error fetching quote:', error));
 }
 
 getRandomQuote(); // Load a quote from the default category when the page is loaded
