5517aa400acbed5c51fdc95680806533cde1d0c2
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-03-03 13:53:59 +0530

    Update script.js

--- tags/script.js
+++ tags/script.js
@@ -5,7 +5,7 @@ function getRandomQuote() {
     .then(response => response.json())
     .then(data => {
       document.getElementById('quote-text').textContent = data.content;
-      document.getElementById('quote-category').textContent = data.tags;
+      document.getElementById('quote-category').textContent = 'Category: ' + data.tags.join(', ');
       document.getElementById('quote-author').textContent = '- ' + data.author;
     })
     .catch(error => console.error('Error fetching quote:', error));
